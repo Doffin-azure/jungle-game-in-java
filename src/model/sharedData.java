6d6ff962ea433133e822f07446a520bdddc4b99b
Weison Pan <124496507+WeisonWEileen@users.noreply.github.com>
2023-05-05 20:08:02 +0800

    Delete sharedData.java

--- src/model/sharedData.java
+++
@@ -1,9 +0,0 @@
-package model;
-import java.util.HashMap;
-import java.util.Map;
-
-import model.*;
-
-public class sharedData {
-    public static Map<Integer, ChessboardPoint> chessboardPointMap = new HashMap<>();
-}
